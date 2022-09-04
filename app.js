4c57fd7897d7ea69b600df0537178d0e8ff76e1d
Wellington Lima <0wellingtonlima1@gmail.com>
2022-09-04 21:09:13 +0100

    usado destructuring na const postTemplates e EventListener adicionado ao window

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
+const postsContainer = document.querySelector('#posts-container');
+
 let page = 1;
 
-const getPost = async () => {
+const getPosts = async () => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`);
   return response.json();
 };
 
 const addPostsIntoDOM = async () => {
-  const posts = await getPost();
-  const postsTemplate = posts.map(item =>`
+  const posts = await getPosts();
+  const postsTemplate = posts.map(({id, title, body}) =>`
   <div class="post">
-    <div class="number">${item.id}</div>
+    <div class="number">${id}</div>
     <div class="post-info">
-      <h2 class="post-title">${item.title}</h2>
-      <p class="post-body">${item.body}</p>
+      <h2 class="post-title">${title}</h2>
+      <p class="post-body">${body}</p>
     </div>
   </div>` 
-  );
+  ).join('');
+
+  postsContainer.innerHTML += postsTemplate;
 };
 
 addPostsIntoDOM();
+
+
+window.addEventListener('scroll', () => {
+  
+})
